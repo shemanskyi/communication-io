d72090bf3f18876d5fe2ba94d1c0b529eb5414ec
shemanskyi <shemanskyi@gmail.com>
2024-01-24 15:34:14 +0200

    cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http').Server(app);
 const PORT = 4000
 const socketIO = require('socket.io')(http, {
     cors: {
-        origin: "*"
+        origin: "https://localhost:5050"
     }
 });
 
